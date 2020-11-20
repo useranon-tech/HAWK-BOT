79b3726ff146912088afca8a74e4dfb4abcaad8b
Dias Gans <74783656+useranon-tech@users.noreply.github.com>
2020-11-20 23:41:12 +0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,122 @@
+﻿exports.menu = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagramlu, whatsapplu, merenung, kapanbotaktif, grupch1, grupch2) => {
+	return `🐔 *MENU ${BotName}* 🐔
+  
+Hi, *${id.split("@s.whatsapp.net")[0]}* HAY 👋️
+Berikut adalah beberapa fitur yang ada pada bot ini!✨
+
+⏳*${tampilTanggal}*
+⏳ *${tampilWaktu}*
+
+❤️ Command / Perintah :
+
+😁FUN😁
+
+➥1.#sticker
+_${BotName} akan mengirimkan stickermu dengan caption #sticker!_
+Perintah: #sticker
+Contoh: kirim gambar dengan caption #sticker   
+
+➥2.#ranime
+_${BotName} akan mengirimkan gambar anime secara random!_
+Contoh: #ranime
+
+➥3.#loli
+_${BotName} akan mengirimkan foto loli UwU secara random_
+Perintah: #loli
+Contoh: #loli
+
+➥4.#pokemon
+_${BotName} akan mengirimkan foto pokemon UwU secara random_
+Perintah: #pokemon
+Contoh: #pokemen
+
+📥DOWNLOADER MENU📥
+
+➥1.#yt [link]
+_${BotName} akan mengirimkan video downloader dari yt sesuai perintah kamu!_
+Contoh: #yt https://youtu.be/imamsyahfa
+
+➥2.#ytmp3 [LINK]
+_${BotName} akan mengirimkan lagu sesuai perintah yang kamu kirim!_
+Contoh: #ytmp3 https://www.youtube.com/channel/UCTpTFbE-A6wssPs1KjWvOIw
+
+➥3.#ig [link]
+_${BotName} akan mengirimkan video/foto ig sesuai perintah yang kamu kirim!_
+Contoh: #ig https://instagram.com/dias_hndrk
+
+➥4.#fb [LINK] <Error>
+_${BotName} akan mengirimkan video/foto fb (tidak bisa export video dari group)!_
+Contoh: #fb https://facebook.com/diaz.hendrik.39
+
+➥5.#twt [LINK]
+_${BotName} akan mengirimkan video/foto twitter sesuai perintah yang kamu kirim!_
+Contoh: #twt https://twitter.com/dias_hndrk
+
+➥6.#tiktok [LINK]
+_${BotName} akan mengirimkan video/foto twitter sesuai perintah yang kamu kirim!_
+Contoh: #tiktok https://vt.tiktok.com/ZSqcTMv3/
+
+🙏SLAMIC🙏
+
+➥1.#quran
+_${BotName} akan mengirimkan ayat-ayat quran secara random!_
+Perintah: #quran
+Contoh: #quran
+
+➥5.#sholat
+_${BotName} akan mengirimkan video/foto twitter sesuai perintah yang kamu kirim!_
+Perintah: #sholat + daerah kamu
+Contoh: #sholat Surabaya
+
+😊INFO😊
+
+➥1.#wiki
+_${BotName} akan mengirimkan hasil dari wikipedia sesuai perintah yang kamu kirim!_
+Perintah: #wiki [perintah]
+Contoh: #wiki apa itu nuklir
+
+➥2.#info
+_${BotName} akan mengirimkan informasi tentang bot!_
+Perintah: #info
+Contoh: #info
+
+➥3.#donasi
+_${BotName} akan mengirimkan informasi tentang bot!_
+Perintah: #donasi
+Contoh: #donasi
+
+➥4.#covid
+_${BotName} akan mengirimkan informasi tentang bot!_
+Perintah: #covid
+Contoh: #covid
+
+♻️ JANGAN  LUPA DONASI AGAR BOT AKTIF TERUS!
+♻️ MAU DONASI? SILAHKAN KETIK #donate
+
+🔊DANGER!!!
+
+✅ Follow akun instagram admin ${instagramlu}
+
+🤧 INFORMASI COVID-19 TERBARU!
+
+😷 POSITIF: *${corohelp.confirmed.value}*
+😄 SEMBUH: *${corohelp.recovered.value}*
+😭 MENINGGAL: *${corohelp.deaths.value}*
+😡 UPDATE: *${corohelp.lastUpdate}*
+
+♨️️ _TETAP JAGA KESEHATAN DAN SELALU PAKAI MASKER!_♨️
+
+📺 *IKLAN* : *INSTAGRAM: @dias_hndrk*
+  
+♨️ GROUP NGOBROL [1] : https://chat.whatsapp.com/HgfzMdZSMJOL7rXHdn3mR0
+
+♨️ CHILL GROUP [2] : https://chat.whatsapp.com/Cj7k0JeEpDVLmGtgFmLK3I
+
+😭 MERENUNG: https://www.instagram.com/dias_hndrk';}
+
+♻️ Mau pasang iklan di *${BotName} ?*
+☎️ WA : *wa.me/+6281211233903?text=pasang+iklan+bang*
+
+  
+( COPYRIGHT BY* ${BotName}* )`
+}
